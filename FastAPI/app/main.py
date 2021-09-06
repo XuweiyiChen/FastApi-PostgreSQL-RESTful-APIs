4227205ab19d4a9b22e5470b43e26462c13ac298
Chen Xuweiyi <chenxuweiyi@ChendeMac-mini.local>
2021-09-06 16:42:23 +0800

    edit readme

--- FastAPI/app/main.py
+++ FastAPI/app/main.py
@@ -17,42 +17,10 @@ def db():
     finally:
         db.close()
 
+# I often use this API to see whether the update is responsive just in case.
 @app.get("/")
 def read_root():
-    return {"Hello": "World 10"}
-
-# @app.post('/device/info')
-# def save_device_info(info: DeviceInfo, db=Depends(db)):
-#     object_in_db = crud.get_device_info(db, info.token)
-#     if object_in_db:
-#         raise HTTPException(400, detail= crud.error_message('This device info already exists'))
-#     return crud.save_device_info(db,info)
-
-# @app.get('/device/info/{token}')
-# def get_device_info(token: str, db=Depends(db)):
-#     info = crud.get_device_info(db,token)
-#     if info:
-#         return info
-#     else:
-#         raise HTTPException(404, crud.error_message('No device found for token {}'.format(token)))
-
-# @app.get('/device/info')
-# def get_all_device_info(db=Depends(db)):
-#     return crud.get_device_info(db)
-
-# @app.post('/configuration')
-# def save_configuration(config: Configuration, db=Depends(db)):
-#     # always maintain one config
-#     # crud.delete_nudges_configuration(db)
-#     return crud.save_nudges_configuration(db, config)
-
-# @app.get('/configuration')
-# def get_configuration(db=Depends(db)):
-#     config = crud.get_nudges_configuration(db)
-#     if config:
-#         return config
-#     else:
-#         raise HTTPException(404, crud.error_message('No configuration set'))
+    return {"Hello": "World 13"}
 
 @app.get('/connectionDict/allsearch')
 def get_connectionDict(db=Depends(db)):
@@ -70,7 +38,7 @@ def get_connectionDict_search(connectionDict: ConnectionDict, db=Depends(db)):
     else:
        raise HTTPException(404, crud.error_message('No connectionId found'))     
 
-@app.get('/connectionDict/delete')
+@app.get('/connectionDict/alldelete')
 def delete_all_connectionDict(db=Depends(db)):
     # only for development
     delete_info = crud.delete_all_connectionDict(db)
@@ -79,7 +47,7 @@ def delete_all_connectionDict(db=Depends(db)):
     else:
        raise HTTPException(404, crud.error_message('Cannot Process Delete. Please Retry')) 
 
-@app.get('/connectionDict/specific_delete')
+@app.get('/connectionDict/delete')
 def delete_specific_connectionDict(widget_id: int, slot: str, connectionid=None, db=Depends(db)):
     # this method is for delete entry in the table, which
     # contains two circumstances: connectionId exists or None
